e339eb883bb6057bf3b1944db27a2a7f3cb61775
amondal <amitmo@mitrmedia.com>
2019-09-25 16:24:49 +0530

    updating graphql with schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
 const app = express();
 
+// Creating middleware
+app.use('/graphql', graphqlHTTP({
+    
+}));
+
+// Listen port
 app.listen(5000, () => {
     console.log('Listening for requests on port 5000');
 });
