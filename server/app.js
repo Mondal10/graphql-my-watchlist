8d6f841a0b361762d4b01e540e08fad3c95d4787
amondal <amitmo@mitrmedia.com>
2019-09-25 17:06:43 +0530

    adding dummy data for querying

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // Creating middleware
 app.use('/graphql', graphqlHTTP({
-    
+    schema
 }));
 
 // Listen port
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+// Dummy data
+const books = [
+    {name:'Immortals of Meluha', genre:'fiction', id:'1'},
+    {name:'The Secret', genre:'personal development', id:'2'},
+    {name:'blah blah', genre:'brruuuuu', id:'3'},
+    {name:'blooh blooh', genre:'trruuuuu', id:'4'},
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -10,4 +18,24 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: { type: GraphQLString}
+            },
+            resolve(parent, args) {
+                // Code to get data from DB/ other source
+                return _.find(books, {id:args.id})
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
