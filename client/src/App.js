cfb7772ad555e967858d653d3df700327d6321d3
Mondal10 <amitmondal111.am@gmail.com>
2019-09-29 16:37:05 +0530

    resolving cors error and binding graphql with react using apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-// Components
+// components
 import BookList from './components/BookList';
 
-// Apollo client setup
+// apollo client setup
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
 });
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div id="main">
-        <h1>Amit's Reading List</h1>
-        <BookList />
-      </div>
-    </ApolloProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Amit's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
-
 export default App;
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-function BookList() {
-  return (
-    <div>
-      <ul id="book-list">
-        <li>Book lists</li>
-      </ul>
-    </div>
-  );
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
+
+class BookList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
