7cd8df6e1807e264f2a08199ae0f8f4f6a57c93d
Mondal10 <amitmondal111.am@gmail.com>
2019-09-29 17:24:26 +0530

    displaying book list

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,22 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+
+  displayBooks() {
+    let data = this.props.data;
+
+    if (data.loading) {
+      return (<div>Loading Books...</div>)
+    } else {
+      return data.books.map(book => <li key={book.id}>{book.name}</li>);
+    }
+  }
+
   render() {
-    console.log(this.props);
     return (
       <div>
         <ul id="book-list">
-          <li>Book name</li>
+          {this.displayBooks()}
         </ul>
       </div>
     );
