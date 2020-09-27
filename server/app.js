2370ca02e48e8f779d778349b6d1537cb95d3e3e
Amit Mondal <amitmondal111.am@gmail.com>
2020-09-27 13:43:28 +0530

    migrating mlab to atlas mongoDB

--- server/app.js
+++ server/app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors());
 
 // Connecting to the database
-const uri = `mongodb://${process.env.MONGO_URI}/graphql-amit-watch-list`;
+// const uri = `mongodb://${process.env.MONGO_URI}/graphql-amit-watch-list`;
+const uri = `mongodb+srv://${process.env.MONGO_URI}@graphql-amit-watch-list.mphpf.mongodb.net/graphql-amit-watch-list?retryWrites=true&w=majority`;
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(uri, { useNewUrlParser: true });
